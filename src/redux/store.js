d786f54b80119a4150e859096b12d40ee476e166
Dmytro Bykanov <75586266+DimByl@users.noreply.github.com>
2021-10-12 16:47:03 +0300

    up7

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,27 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { persistStore, persistReducer } from "redux-persist";
-
-import middleware from "./middleware";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import contactsPersistConfig from "./contacts/contacts-persist-config";
 import contactsReducer from "./contacts/contacts-reducer";
+import logger from "redux-logger";
+
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+
+const middleware = [
+  ...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+  logger,
+];
 
 const store = configureStore({
   reducer: {
